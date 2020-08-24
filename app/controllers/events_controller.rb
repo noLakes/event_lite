ef73deed89a32734c058d6a9a664ba9e34adbb0b
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-08-24 17:03:09 -0400

    added strong params to event contorller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,11 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :location, :description,
+    :date, :time)
+  end
 end
