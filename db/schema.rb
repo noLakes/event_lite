248cac3060fa77eb83414ff68691a14875df89bc
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-08-24 16:55:59 -0400

    added description column to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_194245) do
+ActiveRecord::Schema.define(version: 2020_08_24_205415) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_08_24_194245) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.text "description"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
