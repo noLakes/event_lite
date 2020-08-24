57e2b22d667d27b47fb8dc50f30f804b0fc928ac
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-08-24 16:24:45 -0400

    reconfigured user/event association

--- db/migrate/20200824191503_add_creator_id_to_event.rb
+++
@@ -1,5 +0,0 @@
-class AddCreatorIdToEvent < ActiveRecord::Migration[5.2]
-  def change
-    add_column :events, :creator_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_191503) do
+ActiveRecord::Schema.define(version: 2020_08_24_194245) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -19,7 +19,8 @@ ActiveRecord::Schema.define(version: 2020_08_24_191503) do
     t.time "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "creator_id"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
