f30dfe840fd880adc81d7892deddb28c2bdfcf7b
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-08-26 16:13:50 -0400

    added root to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations'}
   resources :users, :only => [ :show ]
   resources :events
-  
+
+  root "events#index"
+
 end
