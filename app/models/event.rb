f6748eb8db5a389840e71a24a38e3dabfed13760
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-08-27 19:23:40 -0400

    added past+upcoming scopes to event class

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: :user_id
   has_many :attendings
   has_many :attendees, through: :attendings, source: :user
+
+  scope :past, -> { where("time < ?", DateTime.now) }
+  scope :upcoming, -> { where("time > ?", DateTime.now) }
 end
