e225fd8358bb9e961d62402bf884eab9fba9147b
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-08-27 19:53:19 -0400

    added simple nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
+  root "events#index"
   devise_for :users, :controllers => { registrations: 'registrations'}
   resources :users, :only => [ :show ]
   resources :events
-
-  root "events#index"
-
 end
